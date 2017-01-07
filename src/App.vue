ad80f10704f17b0e79e5762a5fc87bc5c5c672e0
Grant Stampfli <mail@gstampfli.com>
2017-01-06 19:52:25 -0800

    white space

--- src/App.vue
+++ src/App.vue
@@ -10,36 +10,36 @@
 </template>
 
 <script>
-import Home from './components/Home.vue'
-import About from './components/about.vue'
-import Tech from './components/tech.vue'
-import Work from './components/work.vue'
-import Contact from './components/contact.vue'
-import Links from './components/links.vue'
+  import Home from './components/Home.vue'
+  import About from './components/about.vue'
+  import Tech from './components/tech.vue'
+  import Work from './components/work.vue'
+  import Contact from './components/contact.vue'
+  import Links from './components/links.vue'
 
-export default {
-  data: {
+  export default {
+    data: {
 
-  },
-  methods: {
-    
-  },
-  components: {
-    'home': Home,
-    'about': About,
-    'tech': Tech,
-    'work': Work,
-    'contact': Contact,
-    'links': Links
-  },
-  mounted: function () {
+    },
+    methods: {
+      
+    },
+    components: {
+      'home': Home,
+      'about': About,
+      'tech': Tech,
+      'work': Work,
+      'contact': Contact,
+      'links': Links
+    },
+    mounted: function () {
 
+    }
   }
-}
 </script>
 
 <style>
-body {
-  font-family: Helvetica, sans-serif;
-}
+  body {
+    font-family: Helvetica, sans-serif;
+  }
 </style>
