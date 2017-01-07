25ebb81df6b323d3e2c276f0602929fa9aba8cfd
Grant Stampfli <mail@gstampfli.com>
2017-01-06 19:11:33 -0800

    setup project, started to build out folder structure, and added ui library for site.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ name }}</h1>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    name: 'Grant Stampfli',
+    title: 'Full Stack Developer'
+  },
+  methods: {
+    
+  },
+  mounted: function () {
+
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
